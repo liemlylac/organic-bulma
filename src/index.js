48a1faaf551f520b8e0a6c6a01d46ad2d6f10dba
liemlylac <liemlylac@gmail.com>
2020-09-25 17:17:55 +0700

    Add action toggle active for navbar-burger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 require('./scss/styles.scss');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarBurgers = document.getElementsByClassName('navbar-burger');
+  if (navbarBurgers.length > 0) {
+    for (let navbarBurger of navbarBurgers) {
+      navbarBurger.addEventListener('click', () => {
+        const target = navbarBurger.getAttribute('data-target');
+        const targetElement = document.getElementById(target);
+
+        targetElement.classList.toggle('is-active');
+      });
+    }
+  }
+});
+
