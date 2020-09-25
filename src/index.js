439ebb33c566c2a86cb104eb87f3c5458da9ec6e
liemlylac <liemlylac@gmail.com>
2020-09-26 00:12:25 +0700

    Adding and using @fortawesome/fontawesome-free

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('./scss/styles.scss');
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 document.addEventListener('DOMContentLoaded', () => {
   const navbarBurgers = document.getElementsByClassName('navbar-burger');
@@ -13,4 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
-
